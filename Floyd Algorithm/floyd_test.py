8315424f1380eccb3eb036341af3b1e2d2e811e5
PingaLCS <sgclau@liverpool.ac.uk>
2023-02-22 21:17:50 +0800

    Add the unit test class

--- Floyd Algorithm/floyd_test.py
+++ Floyd Algorithm/floyd_test.py
@@ -1,33 +1,49 @@
+"""
+This is a unit test for Floyd's algorithm with looping. 
+"""
+# importing the module
 import unittest
-import floyd
 import sys
+import floyd
 
 
 class TestFloyd(unittest.TestCase):
-    def test_floyd(self, input_graph, expected_graph):
-        print(input_graph)
-        print(expected_graph)
-        self.assertEqual(floyd.floyd_algorithm.floyd_looping(
-            input_graph), expected_graph)
+    """
+    This is a class that included the unit test for Floyd's algorithm with looping. 
+    """
+
+    def test_floyd(self):
+        """
+        A test of Floyd's algorithm function with looping.
+        Define the input graph and preset the expected graph.
+        Test the result graph is equal to the input graph or not.
+        """
+        # Define the number of NO_PATH in the graph
+        NO_PATH = sys.maxsize
+        # Define the distance matrix of the graph (test case)
+        input_graph = [[0, 5, NO_PATH, 10],
+                       [NO_PATH, 0, 3, NO_PATH],
+                       [NO_PATH, NO_PATH, 0, 1],
+                       [NO_PATH, NO_PATH, NO_PATH, 0]]
+        # expected result (test case)
+        expected_graph = [[0, 5, 8, 9],
+                          [NO_PATH, 0, 3, 4],
+                          [NO_PATH, NO_PATH, 0, 1],
+                          [NO_PATH, NO_PATH, NO_PATH, 0]]
+        # Get and define the length of the graph
+        MAX_LENGTH = len(input_graph[0])
+
+        # Define object
+        mfloyd = floyd.Floyd()
+        # Result graph
+        result_graph = mfloyd.floyd_looping(input_graph)
+        # print the expected graph
+        print("[Expected Graph] Show the distances between every pair of vertices of the expected graph matrix:")
+        mfloyd.print_solution(expected_graph, MAX_LENGTH)
+
+        # Test the result graph is/not equal to expected graph
+        self.assertEqual(result_graph, expected_graph)
 
 
-if __name__ == '__man__':
-    # Define the number of NO_PATH in the graph
-    NO_PATH = sys.maxsize
-    # Define the distance matrix of the graph
-    graph = [[0, 7, NO_PATH, 8],
-             [NO_PATH, 0, 5, NO_PATH],
-             [NO_PATH, NO_PATH, 0, 2],
-             [NO_PATH, NO_PATH, NO_PATH, 0]]
-    # expected result
-    expected_graph = [[0, 7, 12, 8],
-                      [NO_PATH, 0, 5, 7],
-                      [NO_PATH, NO_PATH, 0, 2],
-                      [NO_PATH, NO_PATH, NO_PATH, 0]]
-    # Get and define the length of the graph
-    MAX_LENGTH = len(graph[0])
-
-    print('Start Unit')
+if __name__ == "__main__":
     unittest.main()
-    m = TestFloyd()
-    m.test_floyd(graph, expected_graph)
